9e749e316d91315cf866b8d4e8764ad92fd719f0
Trung Luu <lhtrung307@gmail.com>
2021-01-20 13:57:29 +0700

    Create simple layout and side bar menu. Create simple input component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { routes } from "routes";
 import Spinner from "components/spinner/Spinner";
+import Layout from "Layout";
 
 function App(): React.ReactElement {
   return (
     <Router>
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          {routes.map(
-            ({ path, pathName, exact, component: RouteComponent }) => (
-              <Route path={path} key={pathName} exact={exact}>
-                {<RouteComponent />}
-              </Route>
-            )
-          )}
-        </Switch>
-      </Suspense>
+      <Layout>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            {routes.map(
+              ({ path, pathName, exact, component: RouteComponent }) => (
+                <Route path={path} key={pathName} exact={exact}>
+                  {<RouteComponent />}
+                </Route>
+              )
+            )}
+          </Switch>
+        </Suspense>
+      </Layout>
     </Router>
   );
 }
