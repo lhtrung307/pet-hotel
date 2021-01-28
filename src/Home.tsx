1c6dba1727617123021fedf87f47aa8fd5b24f0a
trang.phamthithuy <trang.phamthithuy@pycogroup@gmail.com>
2021-01-28 15:08:14 +0700

    resolve comment

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from "react";
+import LoginForm from "modules/loginForm/LoginForm";
 
 function Home(): ReactElement {
-  return <div className="p-4">Home</div>;
+  return <LoginForm />;
 }
 
 export default Home;
--- src/modules/loginForm/LoginForm.tsx
+++ src/modules/loginForm/LoginForm.tsx
@@ -20,16 +20,22 @@ type FormData = {
   password: string;
 };
 export default function LoginForm(): ReactElement {
-  const { register, setValue, handleSubmit, errors } = useForm<FormData>();
-  const onSubmit = handleSubmit((data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
     console.log("🚀 ~ file: LoginForm.tsx ~ line 32 ~ onSubmit ~ data", data);
-  });
+  };
+
   return (
     <Container>
-      <Box as="form" w="360px" p={4} onSubmit={onSubmit}>
-        <Heading paddingBottom={6}>Sign In</Heading>
+      <Box as="form" w="360px" p={4} onSubmit={handleSubmit(onSubmit)}>
+        <Heading pb={6}>Sign In</Heading>
         <FormControl
-          paddingBottom={4}
+          pb={4}
           id="username"
           isRequired
           isInvalid={!!errors.username}
@@ -46,12 +52,10 @@ export default function LoginForm(): ReactElement {
             })}
             name="username"
           ></Input>
-          {errors.username && (
-            <FormErrorMessage>{errors?.username?.message}</FormErrorMessage>
-          )}
+          <FormErrorMessage>{errors?.username?.message}</FormErrorMessage>
         </FormControl>
         <FormControl
-          paddingBottom={4}
+          pb={4}
           id="password"
           isRequired
           isInvalid={!!errors.password}
@@ -65,18 +69,26 @@ export default function LoginForm(): ReactElement {
                 message: "This field is required",
                 value: true,
               },
+              minLength: {
+                message: "Password must be more than 6 letters",
+                value: 6,
+              },
             })}
           ></Input>
-          {errors.password && (
-            <FormErrorMessage>{errors?.password?.message}</FormErrorMessage>
-          )}
+          <FormErrorMessage>{errors?.password?.message}</FormErrorMessage>
         </FormControl>
-        <Flex justifyContent="space-between" paddingBottom={6}>
+        <Flex justifyContent="space-between" pb={6}>
           <Checkbox colorScheme="blue">Remember me</Checkbox>
           <Link>Forgot password?</Link>
         </Flex>
         <Stack direction="row" spacing={4}>
-          <Button colorScheme="blue" variant="solid" size="md" type="submit">
+          <Button
+            colorScheme="blue"
+            variant="solid"
+            size="md"
+            type="submit"
+            disabled={isSubmitting}
+          >
             Login
           </Button>
           <Button colorScheme="blue" variant="outline" size="md">
